9ca622d3e409d2923b6b62f048c759bba161cf08
Tynybek <tynybeko1122@gmail.com>
2024-01-12 22:38:20 +0600

    asdasd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#262a2b" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
-        <meta name="theme-color" content="#3c2f2f"></meta>
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="theme-color" content="#00000"></meta>
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <link
           rel="apple-touch-startup-image"
