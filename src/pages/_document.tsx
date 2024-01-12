3ed0316ae8583438d21e2fad795521e572fe96f1
Tynybek <tynybeko1122@gmail.com>
2024-01-12 13:14:19 +0600

    manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#262a2b" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#3c2f2f"></meta>
